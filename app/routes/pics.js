729fb6e4f98980fddc4fcd758fa635250c87b5ba
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-04-21 14:23:57 -0400

    Data source changed. Posts and pics pulled from reddit

--- app/routes/pics.js
+++ app/routes/pics.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.$.getJSON('https://www.reddit.com/r/pics.json')
+      .then ((res) => {
+        return res.data.children
+    });
+  }
 });
--- app/routes/posts.js
+++ app/routes/posts.js
@@ -2,22 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-   return  [
-      {
-        id: '1',
-        title: 'First',
-        body: 'Test one'
-      },
-      {
-        id: '2',
-        title: 'Second',
-        body: 'Test two'
-      },
-      {
-        id: '3',
-        title: 'Third',
-        body: 'Test three'
-      }
-    ]
+    return Ember.$.getJSON('https://www.reddit.com/r/funny.json')
+      .then((res) => {
+        return res.data.children
+    });
   }
 });
