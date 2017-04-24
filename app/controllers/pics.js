db1417519a0878f964beeacb5a45406f00a65918
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-04-24 13:03:29 -0400

    Order btn fully working. Gio logic used

--- app/controllers/pics.js
+++ app/controllers/pics.js
@@ -3,9 +3,33 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   btnStat: true,
 
+  sortedOrder: Ember.computed('btnStat', function() {
+    const sorted = this.get('model').sort(sorter);
+    if (this.get('btnStat')) {
+      return sorted; 
+    } else {
+      return sorted.reverse(); 
+    }
+  }),
+
   actions: {
     orderPics() {
       this.toggleProperty('btnStat');
     } 
   }
 });
+
+
+
+
+  function sorter(a, b) {
+    let nameA = a.data.title.toUpperCase();
+    let nameB = b.data.title.toUpperCase();
+    if (nameA < nameB) {
+      return -1; 
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  }
